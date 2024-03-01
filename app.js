c4ca80a303e21e0f35a89afc5d4d1e965c3cdbd5
al3xvndra <taal21rq@student.ju.se>
2024-03-01 20:28:48 +0100

    Create the projects page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const data = require("./data.js");
+
 const app = express();
 
 app.engine(
@@ -13,6 +15,14 @@ app.use(express.static("public"));
 
 app.get("/", function (requeste, response) {
   response.render("home.hbs");
+});
+
+app.get("/projects", function (requeste, response) {
+  const model = {
+    projects: data.projects,
+  };
+
+  response.render("projects.hbs", model);
 });
 
 app.listen(8080);
---
+++ data.js
@@ -0,0 +1,17 @@
+exports.projects = [
+  {
+    id: 1,
+    title: "NMD1",
+    category: "graphic design",
+  },
+  {
+    id: 2,
+    title: "NMD2",
+    category: "programming",
+  },
+  {
+    id: 3,
+    title: "NMD3",
+    category: "programming",
+  },
+];
