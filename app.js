03df76f15065aead15cf5920f4719eae2b85c5f9
al3xvndra <taal21rq@student.ju.se>
2024-03-29 16:34:35 +0100

    Error handling for edit page

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ db.run(
   date TEXT)`
 );
 
+db.run(
+  `CREATE TABLE IF NOT EXISTS contact(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  email TEXT,
+  message TEXT)`
+);
+
 function getErrorMessagesForProjects(
   title,
   category,
@@ -65,6 +73,20 @@ function getErrorMessagesForProjects(
   }
   if (date.length == minLength) {
     errorMessages.push("The date field can't be empty.");
+  }
+  return errorMessages;
+}
+
+function getErrorMessagesForContact(name, email, message) {
+  const errorMessages = [];
+  if (name.length == minLength) {
+    errorMessages.push("The title field can't be empty.");
+  }
+  if (email.length == minLength) {
+    errorMessages.push("The category field can't be empty.");
+  }
+  if (message.length == minLength) {
+    errorMessages.push("The description field can't be empty.");
   }
   return errorMessages;
 }
@@ -127,28 +149,15 @@ app.post("/projectCreate", function (request, response) {
   const link = request.body.link;
   const date = request.body.date;
 
-  const errorMessages = [];
-  if (title.length == minLength) {
-    errorMessages.push("The title field can't be empty.");
-  }
-  if (category.length == minLength) {
-    errorMessages.push("The category field can't be empty.");
-  }
-  if (description.length == minLength) {
-    errorMessages.push("The description field can't be empty.");
-  }
-  if (imageURL1.length == minLength) {
-    errorMessages.push("The image field can't be empty.");
-  }
-  if (repository.length == minLength) {
-    errorMessages.push("The repository field can't be empty.");
-  }
-  if (link.length == minLength) {
-    errorMessages.push("The link field can't be empty.");
-  }
-  if (date.length == minLength) {
-    errorMessages.push("The date field can't be empty.");
-  }
+  const errorMessages = getErrorMessagesForProjects(
+    title,
+    category,
+    description,
+    imageURL1,
+    repository,
+    link,
+    date
+  );
 
   if (errorMessages.length == 0) {
     const query =
@@ -211,27 +220,55 @@ app.post("/projectEdit/:id", function (request, response) {
   const repository = request.body.repository;
   const link = request.body.link;
   const date = request.body.date;
-  const query = `UPDATE projects
-  SET title = ?, category = ?, description = ?, imageURL1 = ?, repository = ?, link = ?, date = ? WHERE id = ?;`;
 
-  const values = [
+  const errorMessages = getErrorMessagesForProjects(
     title,
     category,
     description,
     imageURL1,
     repository,
     link,
-    date,
-    id,
-  ];
+    date
+  );
 
-  db.run(query, values, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      response.redirect("/projects");
-    }
-  });
+  if (errorMessages.length == 0) {
+    const query = `UPDATE projects
+  SET title = ?, category = ?, description = ?, imageURL1 = ?, repository = ?, link = ?, date = ? WHERE id = ?;`;
+
+    const values = [
+      title,
+      category,
+      description,
+      imageURL1,
+      repository,
+      link,
+      date,
+      id,
+    ];
+
+    db.run(query, values, function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.redirect("/projects");
+      }
+    });
+  } else {
+    const model = {
+      project: {
+        title,
+        category,
+        description,
+        imageURL1,
+        repository,
+        link,
+        date,
+      },
+      id,
+      errorMessages,
+    };
+    response.render("projectEdit.hbs", model);
+  }
 });
 
 app.get("/sure/:id", function (request, response) {
@@ -267,6 +304,60 @@ app.post("/projectDelete/:id", function (request, response) {
 
 app.get("/contact", function (request, response) {
   response.render("contact.hbs");
+});
+
+app.post("/contact", function (request, response) {
+  const name = request.body.name;
+  const email = request.body.email;
+  const message = request.body.message;
+
+  const errorMessages = getErrorMessagesForContact(name, email, message);
+
+  if (errorMessages.length == 0) {
+    const query = "INSERT INTO contact (name, email, message) VALUES (?, ?, ?)";
+    const values = [name, email, message];
+
+    db.run(query, values, function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.redirect("/thankyou");
+      }
+    });
+  } else {
+    const model = {
+      errorMessages,
+      name,
+      email,
+      message,
+    };
+    response.render("contact.hbs", model);
+  }
+});
+
+app.get("/messages", function (request, response) {
+  const query = "SELECT * FROM contact ORDER BY id DESC";
+
+  db.all(query, function (error, contact) {
+    if (error) {
+      console.log(error);
+      const model = {
+        errorDB: true,
+      };
+      response.render("messages.hbs", model);
+    } else {
+      const model = {
+        contact,
+        errorDB: false,
+      };
+
+      response.render("messages.hbs", model);
+    }
+  });
+});
+
+app.get("/thankyou", function (request, response) {
+  response.render("thankyou.hbs");
 });
 
 app.get("/about", function (request, response) {
